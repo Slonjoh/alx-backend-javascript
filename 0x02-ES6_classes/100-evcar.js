97a11440a53a8f1abae29d1ebaddcb0feccb69df
Slonjoh <tobson11122@gmail.com>
2024-04-25 13:00:57 +0000

    Classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Car from './10-car';
 
 export default class EVCar extends Car {
@@ -6,8 +7,7 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
-  // eslint-disable-next-line
   cloneCar() {
-    return new Car[Symbol.species]();
+    return new Car();
   }
 }
